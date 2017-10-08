6bbff9e07a04417db2da9e0f4e01f32f8d27ad92
Matthew Collier <collier.matthew@gmail.com>
2017-10-08 16:13:07 -0400

    Increase event interval.

--- secondary/config.js
+++ secondary/config.js
@@ -20,7 +20,7 @@ config['ledger-test'].routes = {
 };
 
 // interval to add new events (ms)
-config['ledger-test'].eventInterval = 1000;
+config['ledger-test'].eventInterval = 2500;
 
 config.paths.log = path.join(os.tmpdir(), 'bedrock-ledger-test');
 
