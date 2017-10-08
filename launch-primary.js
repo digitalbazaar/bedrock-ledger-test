fb21a6633e061a4689c58f1d0f853fd8e5c0f6b3
Matthew Collier <collier.matthew@gmail.com>
2017-10-08 13:25:56 -0400

    Use customized AMI.

--- launch-primary.js
+++ launch-primary.js
@@ -3,18 +3,25 @@ const AWS = require('aws-sdk');
 // Load credentials and set region from JSON file
 AWS.config.loadFromPath('./config.json');
 
+// const userData = `
+// #cloud-config
+// apt_upgrade: true
+// runcmd:
+//  - timedatectl set-timezone America/New_York
+//  - curl -sL https://deb.nodesource.com/setup_6.x | bash -
+//  - apt-get install -y nodejs build-essential libkrb5-dev ntp python htop git s3cmd parallel
+//  - curl -sL https://gist.githubusercontent.com/mattcollier/21b8b0a26a3c7cca7f8af2faabdb152a/raw/eb35ee78764946cdb37ac1fd75c0a2eae1a0dc7a/mongo.sh | bash -
+//  - curl -sL https://gist.githubusercontent.com/mattcollier/f4eebff1937b37b585519c0944817553/raw/df7c37824e5262cd64b6b9310fb44421a51a9101/enable_mongo.sh | bash -
+//  - curl -O https://gist.githubusercontent.com/mattcollier/55e976a4a196756682feb99daf41b5a0/raw/mongo-config.js
+//  - sleep 5
+//  - mongo mongo-config.js
+//  - git clone https://github.com/digitalbazaar/bedrock-ledger-test.git
+//  - cd bedrock-ledger-test
+//  - npm install
+//  `;
 const userData = `
 #cloud-config
-apt_upgrade: true
 runcmd:
- - timedatectl set-timezone America/New_York
- - curl -sL https://deb.nodesource.com/setup_6.x | bash -
- - apt-get install -y nodejs build-essential libkrb5-dev ntp python htop git s3cmd parallel
- - curl -sL https://gist.githubusercontent.com/mattcollier/21b8b0a26a3c7cca7f8af2faabdb152a/raw/eb35ee78764946cdb37ac1fd75c0a2eae1a0dc7a/mongo.sh | bash -
- - curl -sL https://gist.githubusercontent.com/mattcollier/f4eebff1937b37b585519c0944817553/raw/df7c37824e5262cd64b6b9310fb44421a51a9101/enable_mongo.sh | bash -
- - curl -O https://gist.githubusercontent.com/mattcollier/55e976a4a196756682feb99daf41b5a0/raw/mongo-config.js
- - sleep 5
- - mongo mongo-config.js
  - git clone https://github.com/digitalbazaar/bedrock-ledger-test.git
  - cd bedrock-ledger-test
  - npm install
@@ -24,7 +31,8 @@ runcmd:
 const ec2 = new AWS.EC2({apiVersion: '2016-11-15'});
 
 let params = {
-  ImageId: 'ami-cd0f5cb6', // amzn-ami-2011.09.1.x86_64-ebs
+  // ImageId: 'ami-cd0f5cb6', // amazon default ubuntu 16.04
+  ImageId: 'ami-69cb0913', // node, mongo base
   InstanceType: 't2.medium',
   KeyName: 'aws-personal',
   MinCount: 1,
