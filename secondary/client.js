dc56df47d5a19e20615e50a96e146da00b28f2ed
Matthew Collier <collier.matthew@gmail.com>
2017-10-09 11:08:04 -0400

    Update secondary.

--- secondary/client.js
+++ secondary/client.js
@@ -6,7 +6,6 @@
 const async = require('async');
 const bedrock = require('bedrock');
 const config = bedrock.config;
-const cfg = config['ledger-test'];
 const logger = require('./logger');
 const request = require('request');
 
@@ -15,13 +14,13 @@ module.exports = api;
 
 api.getGenesis = callback => request({
   method: 'GET',
-  url: `${cfg.primaryBaseUrl}/genesis`,
+  url: `${config['ledger-test'].primaryBaseUrl}/genesis`,
   json: true,
   strictSSL: false
 }, (err, res) => callback(err, res));
 
 api.sendStatus = (options, callback) => {
-  logger.debug('Sending status.', {url: cfg.primaryBaseUrl});
+  logger.debug('Sending status.', {url: config['ledger-test'].primaryBaseUrl});
   return async.auto({
     sendStatus: callback => request({
       body: {
@@ -33,7 +32,7 @@ api.sendStatus = (options, callback) => {
         publicHostname: options.publicHostname
       },
       method: 'POST',
-      url: `${cfg.primaryBaseUrl}/nodes`,
+      url: `${config['ledger-test'].primaryBaseUrl}/nodes`,
       json: true,
       strictSSL: false
     }, callback),
