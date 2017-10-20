1c116e8987b35fde01dc2e2efc35071a1c39dba2
Matthew Collier <collier.matthew@gmail.com>
2017-10-20 15:36:55 -0400

    Increase window size.

--- primary/components/home-component.js
+++ primary/components/home-component.js
@@ -13,7 +13,7 @@ export default {
 function Ctrl($route, $interval) {
   const self = this;
 
-  const eventWindowSize = 5000; // in ms
+  const eventWindowSize = 15000; // in ms
 
   const marker = {};
   self.eventWindow = {};
