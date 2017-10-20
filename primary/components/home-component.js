0302dfb29a17cbc0dc3dc3c63743e0be7717bd81
Matthew Collier <collier.matthew@gmail.com>
2017-10-20 14:54:01 -0400

    Change events per sec calculation.

--- primary/components/home-component.js
+++ primary/components/home-component.js
@@ -13,15 +13,21 @@ export default {
 function Ctrl($route) {
   const self = this;
 
+  const marker = {};
+
   self.blocksPerMinute = (blocks, startTime) => {
     const seconds = (Date.now() - startTime) / 1000;
     const minutes = seconds / 60;
     return (blocks / minutes).toFixed(2);
   };
 
-  self.eventsPerSecond = (events, startTime) => {
-    const seconds = (Date.now() - startTime) / 1000;
-    return (events / seconds).toFixed(2);
+  self.eventsPerSecond = ({total, label}) => {
+    const now = Date.now();
+    const lastMarker = marker[label] || {timeStamp: now, total};
+    marker[label] = {timeStamp: now, total};
+    const newEvents = total - lastMarker.total;
+    const timeDiffSecs = (now - lastMarker.timeStamp) / 1000;
+    return (newEvents / timeDiffSecs).toFixed(2);
   };
 
   self.refresh = () => {
