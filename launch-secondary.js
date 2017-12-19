d88512f6955a726b70d9c05ea02df2a06c288c85
Matthew Collier <collier.matthew@gmail.com>
2017-12-18 19:15:02 -0500

    Update AMI.

--- launch-secondary.js
+++ launch-secondary.js
@@ -20,7 +20,7 @@ const ec2 = new AWS.EC2({apiVersion: '2016-11-15'});
 
 let params = {
   // ImageId: 'ami-cd0f5cb6', // amazon default ubuntu 16.04
-  ImageId: 'ami-69cb0913', // node, mongo base
+  ImageId: 'ami-ebfd8191', // node, mongo base
   InstanceType: 't2.medium',
   KeyName: 'aws-personal',
   IamInstanceProfile: {
