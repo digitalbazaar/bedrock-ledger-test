0882c2929ee45752fa555df8d8e9fdd8b8699e28
Matthew Collier <collier.matthew@gmail.com>
2018-01-10 23:32:56 -0500

    Update client.

--- secondary/client.js
+++ secondary/client.js
@@ -27,15 +27,7 @@ api.sendStatus = ({label, ledgerNodeId, publicHostname}, callback) => {
     ledgerNode: callback =>
       brLedgerNode.get(null, ledgerNodeId, callback),
     latestSummary: ['ledgerNode', (results, callback) =>
-      results.ledgerNode.storage.blocks.getLatestSummary((err, result) => {
-        if(err) {
-          return callback(err);
-        }
-        if(result.eventBlock.block.consensusProof) {
-          delete result.eventBlock.block.consensusProof;
-        }
-        callback(null, result);
-      })],
+      results.ledgerNode.storage.blocks.getLatestSummary(callback)],
     eventsOutstanding: ['ledgerNode', (results, callback) =>
       results.ledgerNode.storage.events.getCount({consensus: false}, callback)],
     eventsTotal: ['ledgerNode', (results, callback) =>
