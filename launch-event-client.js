9c5bc52253611d6151632df981264ae36a741bbe
Matthew Collier <collier.matthew@gmail.com>
2018-01-18 10:56:28 -0500

    Update primary address.

--- launch-event-client.js
+++ launch-event-client.js
@@ -20,7 +20,8 @@ const ec2 = new AWS.EC2({apiVersion: '2016-11-15'});
 let params = {
   // ImageId: 'ami-cd0f5cb6', // amazon default ubuntu 16.04
   ImageId: 'ami-ebfd8191', // node, mongo base
-  InstanceType: 't2.micro',
+  // InstanceType: 't2.micro',
+  InstanceType: 'm5.xlarge',
   KeyName: 'aws-personal',
   IamInstanceProfile: {
     Arn: 'arn:aws:iam::526237877329:instance-profile/bedrock-ledger-node'
--- secondary/config.js
+++ secondary/config.js
@@ -14,7 +14,7 @@ const roles = config.permission.roles;
 
 config['ledger-test'] = {};
 
-config['ledger-test'].primaryHost = 'ip-172-31-0-137.ec2.internal';
+config['ledger-test'].primaryHost = 'ip-172-31-0-49.ec2.internal';
 config['ledger-test'].primaryBaseUrl =
   `https://${config['ledger-test'].primaryHost}:18443/ledger-test`;
 
