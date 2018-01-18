71843e403c0bcee95e96f28a9cb3f582cca06077
Matthew Collier <collier.matthew@gmail.com>
2018-01-18 14:28:23 -0500

    Add clone.

--- cc.sh
+++ cc.sh
@@ -2,6 +2,7 @@
 # clone continuity
 cd node_modules
 rm -rf bedrock-ledger-consensus-continuity
+git clone https://github.com/digitalbazaar/bedrock-ledger-consensus-continuity.git
 cd bedrock-ledger-consensus-continuity
 git checkout $1
 cd ../..
