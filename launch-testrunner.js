e8357d295c05f5043d866144e5a0fa0c7c04e6e4
Matthew Collier <collier.matthew@gmail.com>
2018-01-29 18:23:45 -0500

    Use endorse-special-switches.

--- launch-testrunner.js
+++ launch-testrunner.js
@@ -10,7 +10,7 @@ runcmd:
  - echo 127.0.0.1 bedrock.local >> /etc/hosts
  - git clone https://github.com/digitalbazaar/bedrock-ledger-consensus-continuity.git
  - cd bedrock-ledger-consensus-continuity
- - git checkout byzantine-handling
+ - git checkout endorse-special-switches
  - npm install
  - cd test
  - npm install
