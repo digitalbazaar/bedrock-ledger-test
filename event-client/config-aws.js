3e297fe10d2f6769fb62f703277c0204a3c522ac
Matthew Collier <collier.matthew@gmail.com>
2018-01-29 23:54:48 -0500

    Update primary IP.

--- event-client/config-aws.js
+++ event-client/config-aws.js
@@ -7,4 +7,4 @@ const config = require('bedrock').config;
 
 config.server.port = 18443;
 config.server.httpPort = 18080;
-config['ledger-test'].primaryHost = 'ip-172-31-0-202.ec2.internal';
+config['ledger-test'].primaryHost = 'ip-172-31-0-10.ec2.internal';
