4d194fb86d14667e2199352e309d7c35684443d8
Matthew Collier <collier.matthew@gmail.com>
2018-01-29 23:14:17 -0500

    Update primary IP.

--- secondary/config.js
+++ secondary/config.js
@@ -14,7 +14,7 @@ const roles = config.permission.roles;
 
 config['ledger-test'] = {};
 
-config['ledger-test'].primaryHost = 'ip-172-31-0-36.ec2.internal';
+config['ledger-test'].primaryHost = 'ip-172-31-0-73.ec2.internal';
 config['ledger-test'].primaryBaseUrl =
   `https://${config['ledger-test'].primaryHost}:18443/ledger-test`;
 
