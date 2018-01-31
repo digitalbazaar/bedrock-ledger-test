48dd0e7b01d426a96ea1bde59004a5f079c169ef
Matthew Collier <collier.matthew@gmail.com>
2018-01-31 14:58:17 -0500

    Use t2.small for event client.

--- launch-event-client.js
+++ launch-event-client.js
@@ -39,8 +39,8 @@ if(execute) {
   let params = {
     // ImageId: 'ami-cd0f5cb6', // amazon default ubuntu 16.04
     ImageId: 'ami-44b59d3e', // ledger2.0
-    // InstanceType: 't2.micro',
-    InstanceType: 'm5.xlarge',
+    InstanceType: 't2.small',
+    // InstanceType: 'm5.xlarge',
     KeyName: 'aws-personal',
     IamInstanceProfile: {
       Arn: 'arn:aws:iam::526237877329:instance-profile/bedrock-ledger-node'
