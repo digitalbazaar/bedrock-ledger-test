b26e9ff527808e4add11ff2c199bd0a0c055bcbc
Matthew Collier <collier.matthew@gmail.com>
2018-02-05 21:38:35 -0500

    Use m5.xlarge.

--- launch-secondary.js
+++ launch-secondary.js
@@ -48,8 +48,8 @@ if(execute) {
     ImageId: 'ami-44b59d3e', // ledger2.0
     // InstanceType: 't2.medium',
     // InstanceType: 'c5.large',
-    InstanceType: 'r4.large',
-    // InstanceType: 'm5.xlarge',
+    // InstanceType: 'r4.large',
+    InstanceType: 'm5.xlarge',
     KeyName: 'aws-personal',
     IamInstanceProfile: {
       Arn: 'arn:aws:iam::526237877329:instance-profile/bedrock-ledger-node'
