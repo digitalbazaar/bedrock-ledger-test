de1c45520ee73b144458f1d208499cd3b85315bb
Matthew Collier <collier.matthew@gmail.com>
2018-02-19 16:34:30 -0500

    Remove maxDepth config.

--- secondary/config.js
+++ secondary/config.js
@@ -26,8 +26,6 @@ config['ledger-test'].routes = {
   blocks: '/ledger-test/nodes/:ledgerNodeId/blocks'
 };
 
-config['ledger-consensus-continuity'].gossip.maxDepth = 3;
-
 // c.pushComputed('scheduler.jobs', () => ({
 //   id: `bedrock-ledger-test.stats.logStats`,
 //   type: `bedrock-ledger-test.stats.logStats`,
