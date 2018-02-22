9704a491a5e7d5ba01b4f102a1001672fad3f155
Matthew Collier <collier.matthew@gmail.com>
2018-02-22 14:34:23 -0500

    Update mongo AMI.

--- launch-mongo.js
+++ launch-mongo.js
@@ -41,7 +41,8 @@ if(execute) {
     // ImageId: 'ami-cd0f5cb6', // amazon default ubuntu 16.04
     // ImageId: 'ami-a09a99d', // mongo 3.4.11
     // InstanceType: 'm5.xlarge',
-    ImageId: 'ami-1d6d7067', // i3 mongo 3.4.11
+    // ImageId: 'ami-1d6d7067', // i3 mongo 3.4.11
+    ImageId: 'ami-0266867f', // i3 mongo 3.6
     InstanceType: 'i3.large',
     KeyName: 'aws-personal',
     IamInstanceProfile: {
