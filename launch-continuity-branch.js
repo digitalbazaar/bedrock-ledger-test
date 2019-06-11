343d4e5dfc63be0bbe199a97f916f201dc450a51
Matthew Collier <mcollier@digitalbazaar.com>
2019-06-11 13:20:40 -0400

    Update image.

--- launch-continuity-branch.js
+++ launch-continuity-branch.js
@@ -58,7 +58,7 @@ async function run() {
     const server = await createServer({
       cloudConfig: Buffer.from(continuityBranchConfig).toString('base64'),
       flavor: '14cb1106-0d17-48d4-9b85-90d743ccae06', // branch-test
-      image: 'fee15bca-2898-4ce2-bd0e-f085a2a29621', // node10base
+      image: '27395632-3a9f-4a62-803a-c5cf0b702ba8', // test-base-mongo4
       keyname: 'matt-rsa',
       name: `continuity-${uuid()}`,
       networks: [{uuid: 'e78a0d0d-dab0-4e9d-b4f1-f451ff32c6a9'}],
