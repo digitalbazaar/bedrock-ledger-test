b7d11f867bf26a37d71461cb399ee05ba6c916b1
Matthew Collier <mcollier@digitalbazaar.com>
2020-11-13 20:56:59 -0500

    Update continuity branch image.

--- launch-continuity-branch.js
+++ launch-continuity-branch.js
@@ -30,8 +30,8 @@ async function run() {
       cloudConfig: Buffer.from(continuityBranchConfig).toString('base64'),
       // branch-test
       flavor: '14cb1106-0d17-48d4-9b85-90d743ccae06',
-      // ledger-test-node14-mongo4.2-redis5
-      image: '45533be0-3941-44ef-ac43-4cb190059e18',
+      // ledger-test-node14.15-mongo4.2-redis5
+      image: 'be97b75a-19dd-422c-87f1-11c20b5797b1',
       name: `continuity-${uuid()}`,
       networks: [{uuid: 'e78a0d0d-dab0-4e9d-b4f1-f451ff32c6a9'}],
       securityGroups: [{name: 'default'}],
